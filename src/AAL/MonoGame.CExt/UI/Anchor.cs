66a4069e6c6b8dfadbd76a2c58a884d0aeecb180
Amar Persaud <tehspartaa@gmail.com>
2022-06-06 21:10:56 -0400

    Add constants for some basic anchor situations

--- src/AAL/MonoGame.CExt/UI/Anchor.cs
+++ src/AAL/MonoGame.CExt/UI/Anchor.cs
@@ -6,9 +6,39 @@ namespace MonoGame.CExt.UI
 {
     public struct Anchor
     {
+        /// <summary>
+        /// If left is anchored
+        /// </summary>
         public bool Top;
+
+        /// <summary>
+        /// If right is anchored
+        /// </summary>
         public bool Right;
+
+        /// <summary>
+        /// If bottom side is anchored
+        /// </summary>
         public bool Bottom;
+
+        /// <summary>
+        /// If left side is anchored
+        /// </summary>
         public bool Left;
+
+        /// <summary>
+        /// Default UI control anchors. Top and left side are anchored.
+        /// </summary>
+        public static Anchor Default = new Anchor { Top = true, Bottom = false, Right = false, Left = true };
+
+        /// <summary>
+        /// No anchors. Identical to new Anchor struct
+        /// </summary>
+        public static Anchor None = new Anchor{ Top = false, Bottom = false, Right = false, Left = false };
+
+        /// <summary>
+        /// All sides anchored.
+        /// </summary>
+        public static Anchor All = new Anchor { Top = true, Bottom = true, Right = true, Left = true };
     }
 }
