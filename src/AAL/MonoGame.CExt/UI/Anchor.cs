8f73ed4ef3b6929ada052824119a7383d63a34d5
Amar Persaud <tehspartaa@gmail.com>
2022-06-06 20:49:58 -0400

    Add side and anchor classes for anchoring edges to parent control

---
+++ src/AAL/MonoGame.CExt/UI/Anchor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame.CExt.UI
+{
+    public struct Anchor
+    {
+        public bool Top;
+        public bool Right;
+        public bool Bottom;
+        public bool Left;
+    }
+}
---
+++ src/AAL/MonoGame.CExt/UI/Side.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame.CExt.UI
+{
+    public enum Side
+    {
+        Top,
+        Right,
+        Bottom,
+        Left,
+        None
+    }
+}
