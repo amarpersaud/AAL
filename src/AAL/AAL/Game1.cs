178966ae3495eb9c9bd5f63fbd50baa41fe716c2
Amar Persaud <tehspartaa@gmail.com>
2022-06-11 19:18:08 -0400

    Add basic button class and some testing code

--- src/AAL/AAL/Game1.cs
+++ src/AAL/AAL/Game1.cs
@@ -3,7 +3,10 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.CExt.Input;
 using MonoGame.CExt.Extensions;
+using MonoGame.CExt.UI;
+using MonoGame.CExt.Utility;
 using System;
+using MonoGame.CExt.Sprites;
 
 namespace AAL
 {
@@ -13,6 +16,13 @@ namespace AAL
         private SpriteBatch _spriteBatch;
         private SpriteFont spf;
         private InputHelper ih = new InputHelper();
+
+        public Sprite whiteRect;
+
+        public UIHandler uih;
+
+        Button uic;
+        Button uic2;
 
         public Game1()
         {
@@ -31,13 +41,140 @@ namespace AAL
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             spf = Content.Load<SpriteFont>("Arial");
+
+            Texture2D wr = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            Color[] col = new Color[] { Color.White };
+            wr.SetData<Color>(col);
+            whiteRect = new Sprite(wr);
+
+
+            uih = new UIHandler(new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight));
+
+            uic = new Button(whiteRect, whiteRect);
+            uic.Font = spf;
+
+            uih.Children.Add(uic);
+
+            uic.Parent = uih;
+
+            uic.X = 10;
+            uic.Y = 10;
+            uic.Width = 200;
+            uic.Height = 60;
+
+            uic2 = new Button(whiteRect, whiteRect);
+            uic2.Font = spf;
+
+            uic.Children.Add(uic2);
+
+            uic2.Parent = uic;
+
+            uic2.X = 10;
+            uic2.Y = 10;
+            uic2.Width = 180;
+            uic2.Height = 40;
+            uic2.BackgroundColor = Color.Pink;
+            uic2.PressedColor = Color.Brown;
+
+
+            //uic2.Anchor = Anchor.None;
+
+            uic2.SetAnchor(Side.Left);
+            uic2.SetAnchor(Side.Right);
+            //uic2.SetAnchor(Side.Top);
+            uic2.SetAnchor(Side.Bottom);
+
+
+
+            uic2.Text = "Two door";
+            uic2.Text = "Two door";
         }
 
+        int i = 0;
         protected override void Update(GameTime gameTime)
         {
             ih.Update();
             if (ih.ExitRequested)
                 Exit();
+            
+            uih.Update(gameTime, ih);
+
+            int f = 1;
+
+            if (ih.IsCurPress(Keys.Right))
+            {
+                i++;
+                if(i%f == 0)
+                {
+                    uic.Width++;
+                }
+            }
+            else if (ih.IsCurPress(Keys.Left))
+            {
+                i++;
+                if(i%f == 0 && uic.Width > 0)
+                {
+                    uic.Width--;
+                }
+            }
+
+
+            if (ih.IsCurPress(Keys.Down))
+            {
+                i++;
+                if (i % f == 0)
+                {
+                    uic.Height++;
+                }
+            }
+            else if (ih.IsCurPress(Keys.Up))
+            {
+                i++;
+                if (i % f == 0 && uic.Height > 0)
+                {
+                    uic.Height--;
+                }
+            }
+
+
+
+
+            if (ih.IsCurPress(Keys.D))
+            {
+                i++;
+                if (i % f == 0)
+                {
+                    uic.Left++;
+                }
+            }
+            else if (ih.IsCurPress(Keys.A))
+            {
+                i++;
+                if (i % f == 0 && uic.Width > 0)
+                {
+                    uic.Left--;
+                }
+            }
+
+
+            if (ih.IsCurPress(Keys.S))
+            {
+                i++;
+                if (i % f == 0)
+                {
+                    uic.Top++;
+                }
+            }
+            else if (ih.IsCurPress(Keys.W))
+            {
+                i++;
+                if (i % f == 0 && uic.Height > 0)
+                {
+                    uic.Top--;
+                }
+            }
+
+
 
             base.Update(gameTime);
         }
@@ -49,6 +186,7 @@ namespace AAL
             _spriteBatch.Begin();
 
             //Draw
+            uih.Draw(_spriteBatch);
 
             _spriteBatch.End();
 
---
+++ src/AAL/MonoGame.CExt/UI/Button.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.CExt.Input;
+using MonoGame.CExt.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame.CExt.UI
+{
+    public class Button : UIControl
+    {
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                if (_text != null)
+                {
+                    TextDimensions = this.Font.MeasureString(Text);
+                }
+            }
+        }
+
+        private Vector2 TextDimensions;
+
+        public Point TextPosition
+        {
+            get
+            {
+                return (this.ScreenLocation.ToVector2() + (this.Size.ToVector2() / 2.0f) - (TextDimensions / 2.0f)).ToPoint();
+            }
+        }
+
+        private string _text;
+
+        public Color PressedColor;
+        public Sprite PressedTexture;
+
+        public Button(Sprite BaseTexture, Sprite PressedTexture) : base()
+        {
+            this.BackgroundTexture = BaseTexture;
+            this.PressedTexture = PressedTexture;
+
+            PressedColor = Color.Green;
+            BackgroundColor = Color.Gray;
+        }
+
+        public override void Update(GameTime gameTime, double timeScale, InputHelper ih, UIHandler uih)
+        {
+            base.Update(gameTime, timeScale, ih, uih);
+
+            if (Enabled)
+            {
+
+                //Children should be null, but update them if there are
+                if (Children != null)
+                {
+                    foreach (var c in Children)
+                    {
+                        c.Update(gameTime, timeScale, ih, uih);
+                    }
+                }
+            }
+        }
+
+        public override void Draw(SpriteBatch sb)
+        {
+            base.Draw(sb);
+
+            if (Visible)
+            {
+                if (Hover & !Pressed)
+                {
+                    sb.Draw(this.BackgroundTexture.BaseTexture, this.ScreenBounds, BackgroundColor);
+                }
+                else
+                {
+                    sb.Draw(this.PressedTexture.BaseTexture, this.ScreenBounds, PressedColor);
+                }
+
+                if (Text != null && Font != null)
+                {
+                    sb.DrawString(Font, Text, TextPosition.ToVector2(), ForeColor);
+                }
+
+                //Children should be null, but draw them if there are
+                if (Children != null)
+                {
+                    foreach (var c in Children)
+                    {
+                        c.Draw(sb);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
