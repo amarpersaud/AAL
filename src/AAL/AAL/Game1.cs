431d874ba14fc16c994061b5af7f7a92030737e6
Amar Persaud <tehspartaa@gmail.com>
2022-06-15 21:49:18 -0400

    Testing code for some layout testing

--- src/AAL/AAL/Game1.cs
+++ src/AAL/AAL/Game1.cs
@@ -21,6 +21,8 @@ namespace AAL
 
         public UIHandler uih;
 
+        private RasterizerState _rasterizerState = new RasterizerState() { ScissorTestEnable = true, MultiSampleAntiAlias = false };
+
         Button uic;
         Button uic2;
 
@@ -57,40 +59,32 @@ namespace AAL
 
             uic.Parent = uih;
 
-            uic.X = 10;
+            uic.X = 50;
             uic.Y = 10;
-            uic.Width = 200;
-            uic.Height = 60;
+            uic.Width = 80;
+            uic.Height = 120;
+
+            uic.Text = "Inbox";
 
             uic2 = new Button(whiteRect, whiteRect);
             uic2.Font = spf;
 
-            uic.Children.Add(uic2);
+            uih.Children.Add(uic2);
 
-            uic2.Parent = uic;
+            uic2.Parent = uih;
 
-            uic2.X = 10;
+            uic2.X = 660;
             uic2.Y = 10;
-            uic2.Width = 180;
-            uic2.Height = 40;
+            uic2.Width = 80;
+            uic2.Height = 120;
             uic2.BackgroundColor = Color.Pink;
             uic2.PressedColor = Color.Brown;
 
 
-            //uic2.Anchor = Anchor.None;
+            uic2.Text = "Outbox";
 
-            uic2.SetAnchor(Side.Left);
-            uic2.SetAnchor(Side.Right);
-            //uic2.SetAnchor(Side.Top);
-            uic2.SetAnchor(Side.Bottom);
-
-
-
-            uic2.Text = "Two door";
-            uic2.Text = "Two door";
         }
 
-        int i = 0;
         protected override void Update(GameTime gameTime)
         {
             ih.Update();
@@ -99,94 +93,32 @@ namespace AAL
             
             uih.Update(gameTime, ih);
 
-            int f = 1;
-
-            if (ih.IsCurPress(Keys.Right))
-            {
-                i++;
-                if(i%f == 0)
-                {
-                    uic.Width++;
-                }
-            }
-            else if (ih.IsCurPress(Keys.Left))
-            {
-                i++;
-                if(i%f == 0 && uic.Width > 0)
-                {
-                    uic.Width--;
-                }
-            }
-
-
-            if (ih.IsCurPress(Keys.Down))
-            {
-                i++;
-                if (i % f == 0)
-                {
-                    uic.Height++;
-                }
-            }
-            else if (ih.IsCurPress(Keys.Up))
-            {
-                i++;
-                if (i % f == 0 && uic.Height > 0)
-                {
-                    uic.Height--;
-                }
-            }
-
-
-
-
-            if (ih.IsCurPress(Keys.D))
-            {
-                i++;
-                if (i % f == 0)
-                {
-                    uic.Left++;
-                }
-            }
-            else if (ih.IsCurPress(Keys.A))
-            {
-                i++;
-                if (i % f == 0 && uic.Width > 0)
-                {
-                    uic.Left--;
-                }
-            }
-
-
-            if (ih.IsCurPress(Keys.S))
-            {
-                i++;
-                if (i % f == 0)
-                {
-                    uic.Top++;
-                }
-            }
-            else if (ih.IsCurPress(Keys.W))
-            {
-                i++;
-                if (i % f == 0 && uic.Height > 0)
-                {
-                    uic.Top--;
-                }
-            }
-
 
 
             base.Update(gameTime);
         }
 
+
+        Rectangle Desk = new Rectangle(245, 50, 300, 300);
+        Rectangle Fire = new Rectangle(660, 325, 80, 80);
+
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
 
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, _rasterizerState);
 
             //Draw
+
+
+            _spriteBatch.Draw(whiteRect.BaseTexture, Desk, Color.Tan);
+            _spriteBatch.Draw(whiteRect.BaseTexture, Fire, Color.Red);
+            _spriteBatch.DrawString(spf, "Contract", new Vector2(400, 250), Color.White);
+            
             uih.Draw(_spriteBatch);
+
+
 
             _spriteBatch.End();
 
