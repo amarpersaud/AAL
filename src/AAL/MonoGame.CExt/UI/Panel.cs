18aa51c81d2ac1854a26d95e98fc6950854edb93
Amar Persaud <tehspartaa@gmail.com>
2022-06-22 12:39:02 -0400

    Create panel UIControl

---
+++ src/AAL/MonoGame.CExt/UI/Panel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame.CExt.UI
+{
+    public class Panel : UIControl
+    {
+        public bool ScrollX;
+        public bool ScrollY;
+
+        public Panel() : base() {
+            
+        }
+
+
+    }
+}
