e670dcf155805125f72a4fd2f6e7fd760ae95d56
Amar Persaud <tehspartaa@gmail.com>
2022-06-25 01:40:23 -0400

    Add scrolling to panel

--- src/AAL/MonoGame.CExt/UI/Panel.cs
+++ src/AAL/MonoGame.CExt/UI/Panel.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.CExt.Extensions;
+using MonoGame.CExt.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,18 +16,118 @@ namespace MonoGame.CExt.UI
         /// <summary>
         /// True if X scroll bar is enabled. Only possible for overflow auto or scroll.
         /// </summary>
-        public bool ScrollXEnabled => ((this.Overflow == UIOverflow.Auto) && (this.InnerRect.Width < this.GetContentBounds().Width)) || this.Overflow == UIOverflow.Scroll ;
+        public bool ScrollXEnabled => ((this.Overflow == UIOverflow.Auto) && (this.InnerRect.Width < this.GetContentBounds().Width)) || this.Overflow == UIOverflow.Scroll;
 
         /// <summary>
         /// True if Y scroll bar is enabled. Only possible for overflow auto or scroll.
         /// </summary>
-        public bool ScrollYEnabled => ((this.Overflow == UIOverflow.Auto) && (this.InnerRect.Height < this.GetContentBounds().Height)) || this.Overflow == UIOverflow.Scroll ;
+        public bool ScrollYEnabled => ((this.Overflow == UIOverflow.Auto) && (this.InnerRect.Height < this.GetContentBounds().Height)) || this.Overflow == UIOverflow.Scroll;
+
+        /// <summary>
+        /// Maximum scrolling amount
+        /// </summary>
+        public Point MaxChildOffset
+        {
+            get
+            {
+                //Get bounds of content
+                Rectangle ContentsScreenRect = GetContentBounds();
+                
+                //Return difference so that bottom of content will match to bottom of the inner rect
+                return ContentsScreenRect.Size - this.InnerRect.Size;
+            }
+        }
 
         #endregion Scrolling
 
 
+        /// <summary>
+        /// UI Panel that holds other controls
+        /// </summary>
         public Panel() : base() {
             
+        }
+
+        public override void Update(GameTime gameTime, double timeScale, InputHelper ih, UIHandler uih)
+        {
+            base.Update(gameTime, timeScale, ih, uih);
+
+            float ScrollVelocityScale = 0.1f;
+
+            //This is the selected control
+            if(uih.SelectedControl == this)
+            {
+                //determine if scrolling has occurred regardless of mouse position.
+                Point r = MaxChildOffset;
+
+                //Vertical scrolling
+                if (ScrollYEnabled)
+                {
+                    //Scrolling down -> move contents up
+                    if(ih.MouseScrollWheelVelocityY < 0)
+                    {
+                        //Decrease child offset by scrollwheel velocity and clamp
+                        ChildOffsetY = MathExt.Clamp(ChildOffsetY + (int)(ih.MouseScrollWheelVelocityY * ScrollVelocityScale), -(MaxChildOffset.Y), 0);
+                    }
+                    //Scrolling up -> move contents down
+                    else if (ih.MouseScrollWheelVelocityY > 0)
+                    {
+                        //Increase child offset 
+                        ChildOffsetY = MathExt.Clamp(ChildOffsetY + (int)(ih.MouseScrollWheelVelocityY * ScrollVelocityScale), -(MaxChildOffset.Y), 0);
+                    }
+                }
+                //Horizontal scrolling
+                if(ScrollXEnabled && ih.MouseScrollWheelVelocityX != 0)
+                {
+                    //Scrolling right -> move contents left
+                    if (ih.MouseScrollWheelVelocityX < 0)
+                    {
+                        //Decrease child offset by scrollwheel velocity and clamp
+                        ChildOffsetX = MathExt.Clamp(ChildOffsetX - (int)(ih.MouseScrollWheelVelocityX), -MaxChildOffset.X, 0);
+                    }
+                    //Scrolling left -> move contents right
+                    else if (ih.MouseScrollWheelVelocityX > 0)
+                    {
+                        //Increase child offset 
+                        ChildOffsetX = MathExt.Clamp(ChildOffsetX + (int)(ih.MouseScrollWheelVelocityX), -MaxChildOffset.X, 0);
+                    }
+                }
+            
+            }
+
+            if(Children != null)
+            {
+                foreach(var c in Children)
+                {
+                    c.Update(gameTime, timeScale, ih, uih);
+                }
+            }
+        }
+
+        public override void Draw(SpriteBatch sb)
+        {
+            base.Draw(sb);
+
+            //Draw panel background
+            sb.Draw(this.BackgroundSprite.BaseTexture, this.ScreenBounds, this.BackgroundColor);
+
+            //Copy the current scissor rect so we can restore it after
+            Rectangle currentRect = sb.GraphicsDevice.ScissorRectangle;
+
+            if (Overflow != UIOverflow.Visible)
+            {
+                //Set the current scissor rectangle
+                sb.GraphicsDevice.ScissorRectangle = this.ScreenInnerRect;
+            }
+
+
+            foreach (var c in Children)
+            {
+                c.Draw(sb);
+            }
+
+            sb.GraphicsDevice.ScissorRectangle = currentRect;
+
         }
 
 
@@ -67,12 +170,19 @@ namespace MonoGame.CExt.UI
         /// <summary>
         /// Get bounds of content on screen.
         /// </summary>
-        /// <returns></returns>
-        public Rectangle GetContentScreenBounds()
+        /// <param name="offset">If true, adds parent offset to controls</param>
+        /// <returns>Rectangle with bounds of contents in screen coordinates, and offset by childoffset if offset is true.</returns>
+        public Rectangle GetContentScreenBounds(bool offset)
         {
             var r = GetContentBounds();
             r.Location += this.ScreenLocation;
+            if (offset)
+            {
+                r.X += ChildOffsetX;
+                r.Y += ChildOffsetY;
+            }
             return r;
         }
     }
 }
+
