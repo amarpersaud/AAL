5d120bbf24d6c912e34ef2a221f362b1cd792d06
Amar Persaud <tehspartaa@gmail.com>
2022-06-24 21:19:16 -0400

    Add function to determine if scroll bars visible

--- src/AAL/MonoGame.CExt/UI/Panel.cs
+++ src/AAL/MonoGame.CExt/UI/Panel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,13 +7,72 @@ namespace MonoGame.CExt.UI
 {
     public class Panel : UIControl
     {
-        public bool ScrollX;
-        public bool ScrollY;
+
+        #region Scrolling
+
+        /// <summary>
+        /// True if X scroll bar is enabled. Only possible for overflow auto or scroll.
+        /// </summary>
+        public bool ScrollXEnabled => ((this.Overflow == UIOverflow.Auto) && (this.InnerRect.Width < this.GetContentBounds().Width)) || this.Overflow == UIOverflow.Scroll ;
+
+        /// <summary>
+        /// True if Y scroll bar is enabled. Only possible for overflow auto or scroll.
+        /// </summary>
+        public bool ScrollYEnabled => ((this.Overflow == UIOverflow.Auto) && (this.InnerRect.Height < this.GetContentBounds().Height)) || this.Overflow == UIOverflow.Scroll ;
+
+        #endregion Scrolling
+
 
         public Panel() : base() {
             
         }
 
 
+        /// <summary>
+        /// Get bounds of content relative to the location of the control
+        /// </summary>
+        /// <returns>Rectangle with bounds of contents in local coordinates</returns>
+        public Rectangle GetContentBounds()
+        {
+            Rectangle r = this.InnerRect;
+
+            if(Children != null)
+            {
+                foreach(var c in Children)
+                {
+                    if(c.X < r.X)
+                    {
+                        r.X = c.X;
+                    }
+                    if (c.Y < r.Y)
+                    {
+                        r.Y = c.Y;
+                    }
+
+                    if (c.Right > r.Right)
+                    {
+                        r.Width = c.Right - r.X;
+                    }
+
+                    if (c.Bottom > r.Bottom)
+                    {
+                        r.Height = c.Bottom - r.Y;
+                    }
+                }
+            }
+
+            return r;
+        }
+
+        /// <summary>
+        /// Get bounds of content on screen.
+        /// </summary>
+        /// <returns></returns>
+        public Rectangle GetContentScreenBounds()
+        {
+            var r = GetContentBounds();
+            r.Location += this.ScreenLocation;
+            return r;
+        }
     }
 }
