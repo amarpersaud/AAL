13883f74f884c3bd7ac5bb439e6e0112e1513918
Amar Persaud <tehspartaa@gmail.com>
2022-06-25 01:41:12 -0400

    Add test for panel and desk object

--- src/AAL/AAL/Game1.cs
+++ src/AAL/AAL/Game1.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 using MonoGame.CExt.Input;
 using MonoGame.CExt.Extensions;
 using MonoGame.CExt.UI;
 using MonoGame.CExt.Utility;
-using System;
 using MonoGame.CExt.Sprites;
+using AAL.UI;
 
 namespace AAL
 {
@@ -20,11 +23,12 @@ namespace AAL
         public Sprite whiteRect;
 
         public UIHandler uih;
-
+        
         private RasterizerState _rasterizerState = new RasterizerState() { ScissorTestEnable = true, MultiSampleAntiAlias = false };
 
         Button uic;
         Button uic2;
+        UIContract uicontract;
 
         public Game1()
         {
@@ -83,25 +87,68 @@ namespace AAL
 
             uic2.Text = "Outbox";
 
+            uicontract = new UIContract();
+            uicontract.Font = spf;
+            uicontract.X = 245;
+            uicontract.Y = 50;
+            uicontract.Width = 300;
+            uicontract.Height = 300;
+            uicontract.BackgroundColor = Color.Tan;
+            uicontract.BackgroundSprite = whiteRect;
+            uicontract.Parent = uih;
+
+            uih.Children.Add(uicontract);
+
+
+
+            Panel uip = new Panel();
+            uip.BackgroundSprite = whiteRect;
+            uip.BackgroundColor = Color.Blue;
+
+            uip.X = 100;
+            uip.Y = 100;
+            uip.Width = 300;
+            uip.Height = 300;
+            uip.Parent = uih;
+            uip.Font = spf;
+            uip.Overflow = UIOverflow.Scroll;
+
+            uip.Padding = new Borders { Top = 20, Bottom = 20 };
+
+            for (int i = 0; i < 30; i++)
+            {
+                Button aa1 = new Button(whiteRect, whiteRect);
+                aa1.BackgroundColor = Color.White;
+                aa1.PressedColor = Color.Red;
+                aa1.X = 10;
+                aa1.Y = 10 + (30 * i);
+                aa1.Height = 20;
+                aa1.Width = 280;
+                aa1.Parent = uip;
+                aa1.Font = spf;
+                aa1.Text = String.Format("aa{0}", i+1);
+                uip.Children.Add(aa1);
+            }
+            uih.Children.Add(uip);
+            uih.BringChildToFront(uih.Children.IndexOf(uip));
         }
 
         protected override void Update(GameTime gameTime)
         {
             ih.Update();
             if (ih.ExitRequested)
+            {
                 Exit();
+            }
             
             uih.Update(gameTime, ih);
-
 
 
             base.Update(gameTime);
         }
 
 
-        Rectangle Desk = new Rectangle(245, 50, 300, 300);
         Rectangle Fire = new Rectangle(660, 325, 80, 80);
-
 
         protected override void Draw(GameTime gameTime)
         {
@@ -110,15 +157,9 @@ namespace AAL
             _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, _rasterizerState);
 
             //Draw
-
-
-            _spriteBatch.Draw(whiteRect.BaseTexture, Desk, Color.Tan);
             _spriteBatch.Draw(whiteRect.BaseTexture, Fire, Color.Red);
-            _spriteBatch.DrawString(spf, "Contract", new Vector2(400, 250), Color.White);
-            
             uih.Draw(_spriteBatch);
-
-
+            _spriteBatch.DrawString(spf, "Contract", new Vector2(300, 250), Color.White);
 
             _spriteBatch.End();
 
