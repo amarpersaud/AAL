48579b3ac972db4f310f49d11496edaf090d6af7
Amar Persaud <tehspartaa@gmail.com>
2022-06-29 23:34:31 -0400

    Add helper class for screen dimensions

---
+++ src/AAL/AAL/ScreenSettings.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAL
+{
+    /// <summary>
+    /// Keeps track of window and game screen coordinate systems
+    /// </summary>
+    public class ScreenSettings
+    {
+        /// <summary>
+        /// Dimensions in pixels of the game window
+        /// </summary>
+        public Point WindowDimensions { get; set; }
+
+        /// <summary>
+        /// Dimensions, in the game screen coordinate system, of the screen
+        /// </summary>
+        public Vector2 GameScreenDimensions { get; set; }
+
+        /// <summary>
+        /// Aspect ratio of game screen
+        /// </summary>
+        public float GameAspectRatio => GameScreenDimensions.X / (float)(GameScreenDimensions.Y);
+        
+        /// <summary>
+        /// Aspect ratio of window
+        /// </summary>
+        public float WindowAspectRatio => WindowDimensions.X / (float)(WindowDimensions.Y);
+
+    }
+}
