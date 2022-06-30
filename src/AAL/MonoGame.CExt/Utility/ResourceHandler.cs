578776109b3a29e63391e9a9292faa77f5ec708b
Amar Persaud <tehspartaa@gmail.com>
2022-06-30 15:49:07 -0400

    Add ResourceHandler for holding Sprites and helper classes

---
+++ src/AAL/MonoGame.CExt/Utility/ResourceHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGame.CExt.Input;
+using MonoGame.CExt.Sprites;
+using MonoGame.CExt.Utility;
+using MonoGame.CExt.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.CExt.Utility
+{
+
+    public class ResourceHandler
+    {
+        /// <summary>
+        /// Sprite batch for drawing 
+        /// </summary>
+        public SpriteBatch _sb { get; private set; }
+
+        /// <summary>
+        /// Input helper for handling input
+        /// </summary>
+        public InputHelper _ih { get; private set; }
+
+        /// <summary>
+        /// Frame counter for counting framerate
+        /// </summary>
+        public FrameCounter _fc { get; private set; }
+
+        /// <summary>
+        /// Screen settings, resolution and 
+        /// </summary>
+        public ScreenSettings ScreenSettings { get; private set; }
+
+        /// <summary>
+        /// List of sprites
+        /// </summary>
+        public Dictionary<string, Sprite> Sprites = new Dictionary<string, Sprite>();
+    }
+}
