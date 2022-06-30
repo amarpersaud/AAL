ba4b3ba6e2355439a1b81facb71c646acf7e50ac
Amar Persaud <tehspartaa@gmail.com>
2022-06-30 00:07:07 -0400

    Add position enum

---
+++ src/AAL/MonoGame.CExt/UI/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame.CExt.UI
+{
+    public enum Position
+    {
+        /// <summary>
+        /// Default positioning in internal rectangle. Cannot overlap padding.
+        /// </summary>
+        Default,
+        /// <summary>
+        /// Absolute position relative to viewport
+        /// </summary>
+        Absolute,
+        /// <summary>
+        /// Positioned relative to parent. Similar to default but ignores padding.
+        /// </summary>
+        Relative,
+        /// <summary>
+        /// Inherit positioning from parent. Rarely used.
+        /// </summary>
+        Inherit
+    }
+}
