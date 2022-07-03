e037fc6a421477f8f94ddd3d0ea3e1ad11922215
Amar Persaud <tehspartaa@gmail.com>
2022-07-03 00:12:25 -0400

    Class for random selection from list

---
+++ src/AAL/MonoGame.CExt/Extensions/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoGame.CExt.Extensions
+{
+    public static class CollectionExtensions
+    {
+        private static Random r = new Random();
+        
+        public static T Random<T>(this List<T> list)
+        {
+            return list[r.Next(0,list.Count)];
+        }
+        public static T Random<T>(this List<T> list, int start, int end)
+        {
+            return list[r.Next(start, end)];
+        }
+    }
+}
