45901c0be9325e970ecedd14d05c003331340207
Amar Persaud <tehspartaa@gmail.com>
2022-07-03 23:03:59 -0400

    Add resourcetype enum for special resources

---
+++ src/AAL/AAL/Resources/ResourceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAL.Resources
+{
+    public enum ResourceType
+    {
+        /// <summary>
+        /// Basic material resource
+        /// </summary>
+        Basic,
+
+    }
+}
