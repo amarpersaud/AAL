7103a1534807653467f7025b53ff261413d0431b
Amar Persaud <tehspartaa@gmail.com>
2022-07-03 22:04:13 -0400

    Add map generator

---
+++ src/AAL/AAL/MapGenerator.cs
@@ -0,0 +1,49 @@
+﻿using MonoGame.CExt.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAL
+{
+    public class MapGenerator
+    {
+        private Random r = new Random();
+
+        #region Parameters
+
+        public List<string> PlaceNames = new List<string> { 
+            "New York",
+            "London",
+            "England",
+            "Russia",
+            "Portland",
+            "Hell",
+            "France"
+        };
+
+        public int MinRegions { get; set; }
+        public int MaxRegions { get; set; }
+        public int MinLocationsPerRegion { get; set; }
+        public int MaxLocationsPerRegion { get; set; }
+
+        #endregion Parameters
+
+        public Map GenerateRandomMap()
+        {
+            Map m = new Map();
+            m.Regions = new List<Region>();
+            int numRegions = r.Next(MinRegions, MaxRegions + 1);
+
+            for(int i = 0; i < numRegions; i++)
+            {
+                Region reg = new Region();
+                reg.Name = PlaceNames.Random();
+                m.Regions.Add(reg);
+            }
+
+            return m;
+        }
+    }
+}
