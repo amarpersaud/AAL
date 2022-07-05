e4f3b5c936ba66f98cd0df16ec4511d3692c1c52
Amar Persaud <tehspartaa@gmail.com>
2022-07-04 21:03:16 -0400

    Add GameScreen class for changing scenes

---
+++ src/AAL/MonoGame.CExt/Screen/GameScreen.cs
@@ -0,0 +1,30 @@
+﻿using MonoGame.CExt.UI;
+using MonoGame.CExt.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.CExt.UI
+{
+    public class GameScreen
+    {
+        private ResourceHandler _rh;
+        public string ScreenName { get; private set; }
+        private string SerializedUI { get; set; }
+        private UIHandler uih { get; set; }
+
+        public GameScreen(ResourceHandler rh, string screenName)
+        {
+            this._rh = rh;
+            this.ScreenName = screenName;
+            
+            //TODO: load UI String through rh
+            this.SerializedUI = String.Empty;
+
+            //TODO: Deserialize UI string
+            //uih = 
+        }
+    }
+}
