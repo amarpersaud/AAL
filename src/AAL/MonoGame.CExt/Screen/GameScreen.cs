e44bd48a1fb66d93cc7be8ca7a32ff8b8a95a96b
Amar Persaud <tehspartaa@gmail.com>
2022-07-12 22:04:25 -0400

    Add documentation

--- src/AAL/MonoGame.CExt/Screen/GameScreen.cs
+++ src/AAL/MonoGame.CExt/Screen/GameScreen.cs
@@ -10,11 +10,32 @@ namespace MonoGame.CExt.UI
 {
     public class GameScreen
     {
+        /// <summary>
+        /// Handle to resource handler for input, graphics, etc
+        /// </summary>
         private ResourceHandler _rh;
+
+        /// <summary>
+        /// Name of the screen shown
+        /// </summary>
         public string ScreenName { get; private set; }
+
+        /// <summary>
+        /// String from which the UI is deserialized
+        /// </summary>
         private string SerializedUI { get; set; }
+
+        /// <summary>
+        /// UIHandler root object
+        /// </summary>
         private UIHandler uih { get; set; }
 
+
+        /// <summary>
+        /// Create Game Screen
+        /// </summary>
+        /// <param name="rh"></param>
+        /// <param name="screenName"></param>
         public GameScreen(ResourceHandler rh, string screenName)
         {
             this._rh = rh;
