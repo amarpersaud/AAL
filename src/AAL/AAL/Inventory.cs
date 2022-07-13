c0647db7a15a127a87bbb09cfff0fe0933bf38a9
Amar Persaud <tehspartaa@gmail.com>
2022-07-12 22:04:06 -0400

    Add documentation

--- src/AAL/AAL/Inventory.cs
+++ src/AAL/AAL/Inventory.cs
@@ -9,19 +9,42 @@ namespace AAL
 {
     public class Inventory
     {
-        public int Money { get; set; }
-        public Dictionary<int, int> Resources = new Dictionary<int, int>();
+        /// <summary>
+        /// Amount of money
+        /// </summary>
+        public double Money { get; set; }
 
+        /// <summary>
+        /// Resource amounts: (Resource Id, Resource amount).
+        /// </summary>
+        public Dictionary<int, double> Resources = new Dictionary<int, double>();
+
+        /// <summary>
+        /// Check if inventory contains resource
+        /// </summary>
+        /// <param name="ResourceId">Resource Id of resource to check if inventory contains</param>
+        /// <returns>true if inventory contains resource</returns>
         public bool HasResource(int ResourceId)
         {
             return Resources.Where(x => x.Key == ResourceId).Count() > 0;
         }
+
+        /// <summary>
+        /// Check if inventory contains resource
+        /// </summary>
+        /// <param name="res">Resource to check if inventory contains</param>
+        /// <returns>true if inventory contains resource</returns>
         public bool HasResource(Resource res)
         {
             return Resources.Where(x => x.Key == res.Id).Count() > 0;
         }
 
-        public int GetAmount(int ResourceId)
+        /// <summary>
+        /// Get quantity of resource 
+        /// </summary>
+        /// <param name="ResourceId">Id of resource to get amount of</param>
+        /// <returns>Quantity of resource</returns>
+        public double GetAmount(int ResourceId)
         {
             if (!HasResource(ResourceId))
             {
@@ -30,7 +53,12 @@ namespace AAL
             return Resources[ResourceId];
         }
 
-        public int GetAmount(Resource res)
+        /// <summary>
+        /// Get quantity of resource 
+        /// </summary>
+        /// <param name="res">Resource to get amount of</param>
+        /// <returns>Quantity of resource</returns>
+        public double GetAmount(Resource res)
         {
             if (!HasResource(res))
             {
